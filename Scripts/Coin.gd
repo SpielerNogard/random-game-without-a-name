114dd08800294083fce8a7be6dcacf4f2cab4eca
Christopher Müller <christopher.mueller@be-storaged.com>
2023-01-15 13:36:46 +0100

    added logic to flay to player to coin

--- Scripts/Coin.gd
+++ Scripts/Coin.gd
@@ -1,26 +1,34 @@
 extends AnimatedSprite
 
+var speed = 1
+var player = null
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text
-var value = 1
-var fly_to_player = false
-var collected = false
+func collect(player):
+	player = player
 
-func collect():
-	fly_to_player = true
+func _fly_to_player(delta):
+	if player.global_position.x < global_position.x:
+		global_position.x -= speed*delta
+	
+	if player.global_position.x > global_position.x:
+		global_position.x += speed*delta
+	
+	if player.global_position.y > global_position.y:
+		global_position.y += speed*delta
+	
+	if player.global_position.y < global_position.y:
+		global_position.y -= speed*delta
+	
+	if player.global_position.y == global_position.y and player.global_position.x == global_position.x:
+		player.collect_coin()
+		queue_free()
 
-func _fly_to_player():
-	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	play("default")
-	pass # Replace with function body.
-
-
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if not collected and fly_to_player:
-		_fly_to_player()
+	if player:
+		_fly_to_player(delta)
